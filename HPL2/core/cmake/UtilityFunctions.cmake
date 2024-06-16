ce6147f8209fce68d9779f3bd5679cb8622ed482
jacob <jacob@samochud.home>
2024-06-16 14:00:16 +0200

    CMake: unify dependency lib vars between files (closes #45)

--- HPL2/core/cmake/UtilityFunctions.cmake
+++ HPL2/core/cmake/UtilityFunctions.cmake
@@ -1,23 +1,21 @@
-### build up utility functions
-
 ## Find a prebuiltlib
-## Must set a cache/global var of PREBUILT_PLATFORM_ROOT so this function knows where to find the libraries.
+## Must set a cache/global var of DEPENDENCIES_PLATFORM_LIBS so this function knows where to find the libraries.
 ## the epected structure is simply to have the libs/frameworks in the directory pointed to by that variable.
 ## for multi-arch linux putting libs in two subdirectories of lib and lib64 will allow cmake to find the right ones
 function(FindPrebuiltLibrary result_var libname)
-    if (NOT PREBUILT_PLATFORM_ROOT)
-        message(FATAL_ERROR "Must set PREBUILT_PLATFORM_ROOT before using this function")
+    if (NOT DEPENDENCIES_PLATFORM_LIBS)
+        message(FATAL_ERROR "Must set DEPENDENCIES_PLATFORM_LIBS before using this function")
     endif ()
 
     # check prebuilt directory first
     find_library(${result_var}
             NAMES ${libname}
-            PATHS ${PREBUILT_PLATFORM_ROOT}/lib ${PREBUILT_PLATFORM_ROOT}
+            PATHS ${DEPENDENCIES_PLATFORM_LIBS}/lib ${DEPENDENCIES_PLATFORM_LIBS}
             NO_DEFAULT_PATH)
     # Check system dir
     find_library(${result_var}
             NAMES ${libname})
     if (NOT ${result_var})
-        message(FATAL_ERROR "Could not find library ${libname} in prebuilt folder ${PREBUILT_PLATFORM_ROOT}")
+        message(FATAL_ERROR "Could not find library ${libname} in prebuilt folder ${DEPENDENCIES_PLATFORM_LIBS}")
     endif ()
 endfunction()
